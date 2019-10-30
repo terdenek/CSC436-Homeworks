e1a875c88f7f7f46f54a7819b7b30153c5bf895e
Temuulen Erdenekhuu <bg.genio@gmail.com>
2019-10-29 21:53:07 -0500

    Generated base components
    - Created folders to organize components
    - Generated base components Todo-Item, New-Todo-Item, Todo-List, Dashboard, Page-not-found
    - Generated todo-service

--- hw4/todo-app/src/app/app.module.ts
+++ hw4/todo-app/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { NewTodoItemComponent } from './components/new-todo-item/new-todo-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    PageNotFoundComponent,
+    TodoItemComponent,
+    TodoListComponent,
+    NewTodoItemComponent
   ],
   imports: [
     BrowserModule,
